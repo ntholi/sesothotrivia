37caeedcacdb396f8bb108751fa21cb8ca5b04b6
ntholi <ntholi@live.com>
2016-11-21 13:21:33 +0200

    Style the game activity

--- app/src/main/java/com/nalaneholdings/sesothotrivia/MainActivity.java
+++ app/src/main/java/com/nalaneholdings/sesothotrivia/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -34,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
         user = mFirebaseAuth.getCurrentUser();
         database = FirebaseDatabase.getInstance().getReference();
 
-        if (user == null) {
-            loadLogInView();
-        }
+//        if (user == null) {
+//            loadLogInView();
+//        }
         loadButtons();
 
     }
@@ -60,20 +61,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_logout) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onPlay(View view) {
+        Intent intent = new Intent(this, GamePlayActivity.class);
+        startActivity(intent);
     }
 }
